183b88dfe33026335bf8b682e686ef2434a1d9fa
SalaheddineBensaghir <bensaghir2000@gmail.com>
2024-11-17 10:44:23 +0100

    commit 4

--- customer-service/src/main/java/com/micro/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/micro/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package com.micro.customerservice;
 
+import com.micro.customerservice.config.CustomerConfigParams;
 import com.micro.customerservice.entites.Customer;
 import com.micro.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
